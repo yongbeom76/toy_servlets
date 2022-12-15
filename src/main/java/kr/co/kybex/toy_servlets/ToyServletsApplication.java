9ecc294c14ba0715670198b5c83034834bbb48d3
yongbeom76 <yongbeom76@gmail.com>
2022-12-15 09:29:58 +0900

    first commit

---
+++ src/main/java/kr/co/kybex/toy_servlets/ToyServletsApplication.java
@@ -0,0 +1,13 @@
+package kr.co.kybex.toy_servlets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToyServletsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToyServletsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/co/kybex/toy_servlets/ToyServletsApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.kybex.toy_servlets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToyServletsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
